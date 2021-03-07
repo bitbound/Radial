a152a84ff73242cd994c05434734b67844f19f75
Jared <translucency@outlook.com>
2021-03-07 10:30:49 -0800

    Testing scripting.

---
+++ Radial.Tests/ScriptingTests.cs
@@ -0,0 +1,59 @@
+﻿using Microsoft.CodeAnalysis.CSharp.Scripting;
+using Microsoft.CodeAnalysis.CSharp.Scripting.Hosting;
+using Microsoft.CodeAnalysis.Scripting;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Radial.Models;
+using Radial.Services.Client;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Radial.Tests
+{
+    [TestClass]
+    public class ScriptingTests
+    {
+        [TestMethod]
+        public async Task Test()
+        {
+            var globalObj = new ScriptingGlobals()
+            {
+                PC = new PlayerCharacter()
+                {
+                    Name = "Dude"
+                }
+            };
+
+            var options = ScriptOptions.Default
+                .WithAllowUnsafe(true)
+                .AddReferences(typeof(Program).Assembly)
+                .AddImports(typeof(PlayerCharacter).Namespace);
+
+            var result = await CSharpScript.RunAsync("return PC.Name;", options, globalObj, typeof(ScriptingGlobals));
+            var returnValue = result.ReturnValue;
+
+            Assert.AreEqual("Dude", returnValue);
+
+            result = await result.ContinueWithAsync("DoStuff(); return PC.ChargeCurrent;", options);
+            returnValue = result.ReturnValue;
+            Assert.AreEqual((Int64)1, returnValue);
+
+            result = await result.ContinueWithAsync("DoStuff(); return PC.ChargeCurrent;", options);
+            returnValue = result.ReturnValue;
+            Assert.AreEqual((Int64)2, returnValue);
+
+        }
+
+        public class ScriptingGlobals
+        {
+            public PlayerCharacter PC { get; set; } = new();
+
+            public void DoStuff()
+            {
+                PC.ChargeCurrent++;
+            }
+        }
+    }
+}
