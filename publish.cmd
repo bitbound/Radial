d684fa4003b7280686b06feaa759be8d460db645
Jared Goodwin <translucency@outlook.com>
2023-02-23 21:13:50 -0800

    Update publish.

--- publish.cmd
+++ publish.cmd
@@ -2,8 +2,8 @@ dotnet publish -p:ExcludeApp_Data=true --runtime linux-x64 --configuration Relea
 
 rd /S /Q ".\bin\publish\App_Data\"
 
-scp -r ".\bin\publish\*" "jared@192.168.0.2:/var/www/radial.lucency.co/"
-ssh jared@192.168.0.2 chown -R jared:web /var/www/radial.lucency.co/
-ssh jared@192.168.0.2 chmod -R 775 /var/www/radial.lucency.co/
+scp -r ".\bin\publish\*" "jared@192.168.0.2:/var/www/radial.jaredg.dev/"
+ssh jared@192.168.0.2 chown -R jared:web /var/www/radial.jaredg.dev/
+ssh jared@192.168.0.2 chmod -R 775 /var/www/radial.jaredg.dev/
 echo Publish completed.
 pause
